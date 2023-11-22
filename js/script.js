49e82c5a9fbfef16155974a8835d7625db7fd2d3
muchytil <muchytil@amazon.com>
2023-11-21 18:01:33 -0600

    updated per jen's recommendations

--- js/script.js
+++ js/script.js
@@ -19,5 +19,16 @@ $(document).ready(()=>{
         window.print();
     });
 
+    // Get User's ip address 
+    $.get('https://www.cloudflare.com/cdn-cgi/trace').then(res =>  {
+        var info = res.split('\n');
+        var ip = info[2].split('=')[1];
+        var browser = info[5].split('=')[1].replace("(","").replace(")","").replace("\;","").split(' ');
+        console.log(info);
+        console.log(ip);
+        console.log(browser);
+
+
+        });
     
 });
